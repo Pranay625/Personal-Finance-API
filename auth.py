8033fd11b7fdd735affd50e76f5a938aceb90151
Pranay Rajesh <pranayrajesh625@gmail.com>
2026-09-12 00:34:34 +0530

    JWT Token Complete.

--- auth.py
+++ auth.py
@@ -1,20 +1,30 @@
 from datetime import datetime, timedelta, timezone
+
 from jose import jwt, JWTError
-from fastapi import HTTPException, status, Depends
+from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from db import get_db
+from models import User
+
 
 SECRET_KEY = "Gaara"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+
 oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl = "/user/login"
+    tokenUrl="/users/login"
 )
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
 
     to_encode.update({
         "exp": expire
@@ -23,35 +33,49 @@ def create_access_token(data: dict):
     return jwt.encode(
         to_encode,
         SECRET_KEY,
-        algorithm = ALGORITHM
+        algorithm=ALGORITHM
     )
+
 
 def verify_token(token: str):
     try:
         payload = jwt.decode(
             token,
             SECRET_KEY,
-            algorithm=[ALGORITHM]
+            algorithms=[ALGORITHM]
         )
 
         username = payload.get("sub")
 
         if username is None:
             raise HTTPException(
-                status_code = 401,
-                detail = "Invalid token"
+                status_code=401,
+                detail="Invalid token"
             )
-        
+
         return username
 
     except JWTError:
         raise HTTPException(
-            status_code = 401,
-            detail = "Invalid or expired token"
+            status_code=401,
+            detail="Invalid or expired token"
         )
 
-def get_current_user(
-        token: str = Depends(oauth2_scheme) 
-):
-    return verify_token(token)
 
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    username = verify_token(token)
+
+    user = db.query(User).filter(
+        User.username == username
+    ).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=401,
+            detail="User not found"
+        )
+
+    return user
